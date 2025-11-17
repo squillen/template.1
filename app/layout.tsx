88d5466db5d245c3f1014ce7aae5b85d411a70b3
Sean Quillen <squillen@paypal.com>
2025-11-17 16:07:49 -0600

    Remove geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { CartProvider } from "@/context/cart-context";
 import { PayPalScript } from "@/components/paypal-scripts";
 import { PayPalInjectButtons } from "@/components/paypal-inject-buttons";
@@ -21,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body>
         <PayPalScript />
         <div className="min-h-screen">
           <CartProvider>
